4385edde7e3995bcf686feba0aac70ff97391779
Jaydeep <jdsenwarkr@gmail.com>
2025-12-01 13:18:20 +0530

    comply with v16

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,15 @@ import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
 import { IEvent } from "@/database";
 import { cacheLife } from "next/cache";
+import { events } from "@/lib/constants";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
-  'use cache'
-  cacheLife('hours')
-  const response = await fetch(`${BASE_URL}/api/events`);
-  const { events } = await response.json();
+  "use cache";
+  cacheLife("hours");
+  // const response = await fetch(`${BASE_URL}/api/events`);
+  // const { events } = await response.json();
   return (
     <section>
       <h1 className="text-center">
@@ -23,11 +24,13 @@ const Page = async () => {
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
         <ul className="events">
-          {events && events.length > 0 && events.map((event: IEvent) => (
-            <li key={event.title} className="list-none">
-              <EventCard {...event} />
-            </li>
-          ))}
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li key={event.title} className="list-none">
+                <EventCard {...event} />
+              </li>
+            ))}
         </ul>
       </div>
     </section>
--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
 
   typescript:{
     ignoreBuildErrors:true
-  }
+  },
   cacheComponents: true,
 
 images:{
